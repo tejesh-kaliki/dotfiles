abcfb4d6836ce7087e923fbfc735fba507019532
Tejesh Kaliki <tejesh@reckonsys.com>
2025-06-24 11:06:01 +0530

    Modified wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,21 @@
 local wezterm = require "wezterm"
 
+-- Hook to run after GUI is ready
+wezterm.on('gui-startup', function(cmd)
+  local gui = wezterm.gui
+  local window = gui.window_for_cmd(cmd or {})
+  local display = window:get_display_info()
+  local dpi = display.dpi or 96
+  local font_size = 12.0 * (dpi / 96)
+
+  window:set_config_overrides({
+    font_size = font_size,
+    window_frame = {
+        font_size = font_size
+    }
+  })
+end)
+
 local config = wezterm.config_builder()
 
 -- Set theme
@@ -7,11 +23,5 @@ config.color_scheme = "Catppuccin Mocha"
 
 -- Set default font
 config.font = wezterm.font "JetBrains Mono"
-config.font_size = 15
-
--- Configure tabbar
-config.window_frame = {
-  font_size = 15
-}
 
 return config

81574eb61cd86ed90acd9c5f8cdf44619c0f9e48
Tejesh Kaliki <tejesh.kaliki@gmail.com>
2025-10-02 10:42:16 +0530

    Changed fish config

--- fish/config.fish
+++ fish/config.fish
@@ -63,6 +63,10 @@ if type -q vacuum
     vacuum completion fish | source
 end
 
+if type -q mise
+    mise hook-env --shell=fish | source
+end
+
 # AWS completer
 test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 
